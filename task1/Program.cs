3a3c282013faa2b88214c956834fb1660de0bf93
Igor <pozolotin.com@gmail.com>
2024-01-02 10:25:58 +0300

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Console.WriteLine("как то раз вышло именно вот так!");
+
+// Console.WriteLine("sdfsdf");
+
+int value = 100;
+Console.WriteLine(value);
+
+long mass = -18;
+Console.WriteLine(mass);
+
+double x = 5.55;
+char c ='!';
+bool b = true;
+Console.WriteLine(x);
+Console.WriteLine(c);
+Console.WriteLine(b);
