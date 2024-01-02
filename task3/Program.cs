93f405d530fbdb9919d82814f41ed0ec60a992b8
Igor <pozolotin.com@gmail.com>
2024-01-02 10:27:52 +0300

    added task4

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 0;
+int b = -2;
+
+if(a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = -5;
+int b = -4;
+int c = -3;
+int d = -2;
+int e = 0;
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
